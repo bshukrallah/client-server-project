15460c8e2cbaaed8f63951d0a816bb621f4114f4
bshukrallah <benjamin.shukrallah@live.longwood.edu>
2022-05-08 22:26:36 -0400

    intiial commit

---
+++ NetworkApp/src/Source.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+
+#ifdef _WIN32
+#define _WIN32_WINNT 0x0A00
+#endif
+#define ASIO_STANDALONE
+
+#include <asio.hpp>
+#include <asio/ts/buffer.hpp>
+#include <asio/ts/internet.hpp>
+
+
+int main(){
+	asio::error_code ec;
+
+	//create context
+	asio::io_context context;
+
+	//Get the address to connect to
+	asio::ip::tcp::endpoint endpoint(asio::ip::make_address("93.184.216.34", ec), 80);
+
+	//create a socket, which delivers the implementation
+	asio::ip::tcp::socket socket(context);
+
+	socket.connect(endpoint, ec);
+
+	if (!ec) { std::cout << "Connected!" << std::endl; }
+	else { std::cout << "Failed to connect to address:\n" << ec.message() << std::endl; }
+
+
+	if (socket.is_open()) {
+		std::string sRequest =
+			"GET /index.html HTTP/1.1\r\n"
+			"Host: example.com\r\n"
+			"Connection: close\r\n\r\n";
+
+		socket.write_some(asio::buffer(sRequest.data(), sRequest.size()), ec);
+
+		size_t bytes = socket.available();
+		std::cout << "Bytes Available: " << bytes << std::endl;
+
+		if (bytes > 0)
+		{
+			std::vector<char> vBuffer(bytes);
+			socket.read_some(asio::buffer(vBuffer.data(), vBuffer.size()), ec);
+
+			for (auto c : vBuffer)
+				std::cout << c;
+		}
+
+	}
+
+	return 0;
+}
