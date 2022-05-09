dd9f4dc9c6dec768d3a723aa756cbdb2107443cb
bshukrallah <benjamin.shukrallah@live.longwood.edu>
2022-05-09 00:50:33 -0400

    updates

--- NetworkApp/src/Source.cpp
+++ NetworkApp/src/Source.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <chrono>
 
 #ifdef _WIN32
 #define _WIN32_WINNT 0x0A00
@@ -9,12 +10,36 @@
 #include <asio/ts/buffer.hpp>
 #include <asio/ts/internet.hpp>
 
+std::vector<char> vBuffer(1 * 1024);
+
+void getData(asio::ip::tcp::socket& socket) 
+{
+	socket.async_read_some(asio::buffer(vBuffer.data(), vBuffer.size()),
+		[&](std::error_code ec, std::size_t length)
+		{
+			if (!ec)
+			{
+				std::cout << "\n\nRead " << length << " bytes\n\n";
+
+				for (int i = 0; i < length; i++)
+					std::cout << vBuffer[i];
+
+				getData(socket);
+			}
+		}
+	);
+}
 
 int main(){
 	asio::error_code ec;
 
 	//create context
 	asio::io_context context;
+
+	asio::io_context::work idlework(context);
+
+	std::thread thrContext = std::thread([&]() { context.run(); });
+
 
 	//Get the address to connect to
 	asio::ip::tcp::endpoint endpoint(asio::ip::make_address("93.184.216.34", ec), 80);
@@ -27,8 +52,10 @@ int main(){
 	if (!ec) { std::cout << "Connected!" << std::endl; }
 	else { std::cout << "Failed to connect to address:\n" << ec.message() << std::endl; }
 
+	if (socket.is_open()) 
+	{
+		getData(socket);
 
-	if (socket.is_open()) {
 		std::string sRequest =
 			"GET /index.html HTTP/1.1\r\n"
 			"Host: example.com\r\n"
@@ -36,18 +63,11 @@ int main(){
 
 		socket.write_some(asio::buffer(sRequest.data(), sRequest.size()), ec);
 
-		size_t bytes = socket.available();
-		std::cout << "Bytes Available: " << bytes << std::endl;
+		using namespace std::chrono_literals;
+		std::this_thread::sleep_for(2000ms);
 
-		if (bytes > 0)
-		{
-			std::vector<char> vBuffer(bytes);
-			socket.read_some(asio::buffer(vBuffer.data(), vBuffer.size()), ec);
-
-			for (auto c : vBuffer)
-				std::cout << c;
-		}
-
+		context.stop();
+		if (thrContext.joinable()) thrContext.join();
 	}
 
 	return 0;
